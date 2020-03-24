f9dc0e3b2a196f49488415e80c006b87f2c0a612
yanitzin <yanitzin97@gmail.com>
2020-03-23 23:18:47 -0700

    sin clic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -15,15 +15,15 @@ export class Tab1Page {
   constructor(private  tareasService : TareasService, private router : Router) {
     this.tarea = this.tareasService.getTareas();
   }
-  // view(tarea : any): void {
-  //   const extras: NavigationExtras = {
-  //     queryParams: {
-  //       special: JSON.stringify(tarea)
-  //     }
-  //   };
+  view(tarea : any): void {
+    const extras: NavigationExtras = {
+      queryParams: {
+        special: JSON.stringify(tarea)
+      }
+    };
 
-  //   this.router.navigate(['/view-tareas'], extras);
-  // }
+    this.router.navigate(['/view-tareas'], extras);
+  }
 
   changeStatus(pos: number): void{
     this.tareasService.changeStatus(pos);
